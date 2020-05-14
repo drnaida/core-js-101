3b20cce73e23dc064e28e91dea4546dc3808a5bb
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-05-14 18:57:57 +0300

    feat: implement getCircleCircumference function

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -156,8 +156,8 @@ function unbracketTag(str) {
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function convertToUpperCase(/* str */) {
-  throw new Error('Not implemented');
+function convertToUpperCase(str) {
+  return str.toUpperCase();
 }
 
 /**
@@ -240,8 +240,8 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return value instanceof String;
 }
 
 
