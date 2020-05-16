069c8342b16bf47ee8b2fc6d693f99d27a9af69a
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-05-17 00:21:04 +0300

    feat: implement getRegexPit function

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return new RegExp();
 }
 
 
@@ -54,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return new RegExp('.*p.t.*');
 }
 
 
